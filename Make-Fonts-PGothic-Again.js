a4c1fc9801cde1bac4acd393be4233d27a81cb92
fukuchan <fukuchan@fukuchan.mynetgear.com>
2019-10-12 18:29:20 +0900

    first commit

---
+++ Make-Fonts-PGothic-Again.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Make Fonts PGothic Again
+// @namespace    https://fukuchan.mynetgear.com
+// @version      0.1
+// @description  frame要素内のフォントをMS Pゴシックに戻す
+// @author       fukuchan
+// @match        http://gikoheiki.web.fc2.com/
+// @grant        none
+// ==/UserScript==
+
+// bodyのフォントをMS Pゴシックに戻す
+document.body.style.fontFamily = "MS PGothic";
+
+// frame要素内のbodyについてもMS Pゴシックに戻す
+const frames = document.getElementsByTagName("frame");
+Array.from(frames).forEach(f => {
+    f.onload = () => {
+        f.contentDocument.body.style.fontFamily = "MS PGothic";
+    };
+});
